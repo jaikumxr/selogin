28bf149b6f581a59d966be4b1a8e051c88dbfb0f
Jai Kumar <jaikumxr@gmail.com>
2020-09-06 18:34:31 +0530

    Final

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 var bodyParser = require("body-parser"),
-express = require("express"),
-app = express(),
-mongoose = require("mongoose"),
-passport = require("passport"),
-User = require("./models/user.js"),
-LocalStrategy = require("passport-local"),
-flash = require("connect-flash");
-
-
+    methodOverride = require("method-override"),
+    express = require("express"),
+    app = express(),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    flash = require("connect-flash");
 
 mongoose.connect("mongodb://localhost:27017/se", {
-  useNewUrlParser: true,
-  'useUnifiedTopology': true
+    useNewUrlParser: true,
+    'useUnifiedTopology': true
 });
 mongoose.set('useFindAndModify', false);
 
 var connection = mongoose.connection;
 
+// APP CONFIG
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}));
+app.use(methodOverride("_method"));
 app.use(flash());
+
+// RESTFUL ROUTES
 
 app.use(require("express-session")({
     secret: "jaikumxr",
@@ -30,55 +39,259 @@ app.use(require("express-session")({
 }));
 
 
-app.get("/", function(req, res){
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.get("/", function (req, res) {
     res.redirect("/home");
-})
+});
 
-app.get("/home", isLoggedIn, function(req, res){
-    res.render("home",{
-        currentUser = req.user
+
+//index route
+app.get("/home", isLoggedIn, function (req, res) {
+    res.render("home", {
+        currentUser: req.user,
+
     });
-})
 
-var newUser = {"username":"admin","isAdmin":true,"email":"jai@example.com"};
-var password = "admin";
+});
 
+//admin Dashboard
+app.get("/dashboard", isLoggedIn, function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("dashboard", {
+                users: users
+            });
+        }
+    });
+});
+
+//uncomment this to create initial admin in case an admin deletes all users
+
+var newUser = {"username":"jaikumxr","isAdmin":true,"email":"jai@example.com"};
+var password = "hellojai";
 User.register(newUser, password, function(err,user){
   if(err){
     console.log(err);
   }
 });
 
+//AUTH ROUTES
+
+//show signup form
+app.get("/dashboard/createuser", function (req, res) {
+    res.render("register", {
+    });
+});
+
+//handle signup logic
+app.post("/dashboard/createuser", function (req, res) {
+
+    const {
+        username,
+        password,
+        email,
+        firstName,
+        lastName,
+        confirmpassword
+    } = req.body;
+    let errors = [];
+
+    if (!username || !password || !email) {
+        errors.push({
+            msg: 'Please enter all required fields'
+        });
+    }
+
+    var paswd = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+
+    if (!password.match(paswd)) {
+        errors.push({
+            msg: "Enter a password between 7 to 15 characters which contains at least one numeric digit and a special character."
+        });
+    }
+
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        errors.push({
+            msg: "Please enter a valid email"
+        })
+    }
+
+    if (password != confirmpassword) {
+        errors.push({
+            msg: "Both passwords are different. Kindly enter same password in the Confirm Password field."
+        })
+    }
+
+    if (errors.length > 0) {
+        req.flash("error", errors[0].msg);
+        res.redirect("/dashboard/createuser");
+    } else {
+        var newUser = new User({
+            username: req.body.username
+        });
+        if (req.body.role === "admin") {
+            newUser.isAdmin = "true"
+        }
+        newUser.lastName = lastName;
+        newUser.firstName = firstName;
+        newUser.email = email;
+        User.register(newUser, req.body.password, function (err) {
+            if (err) {
+                console.log(err);
+                return res.redirect("/dashboard/createuser");
+            }
+            req.flash("success", "Successfully created user: " + req.body.username);
+            res.redirect("/dashboard");
+        });
+    }
+});
+
+app.get("/dashboard/:id/edituser", isLoggedIn, function (req, res) {
+    User.findById(req.params.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("edit", {
+                user: foundUser,
+            });
+        }
+    });
+});
+
+app.put("/dashboard/:id", isLoggedIn, function (req, res) {
+
+    const {
+        username,
+        password,
+        email,
+        firstName,
+        lastName,
+        confirmpassword
+    } = req.body;
+    let errors = [];
+
+    if (!username || !password || !email) {
+        errors.push({
+            msg: 'Please enter all required fields'
+        });
+    }
+
+    var paswd = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+
+    if (!password.match(paswd)) {
+        errors.push({
+            msg: "Enter a password between 7 to 15 characters which contains at least one numeric digit and a special character."
+        });
+    }
+
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        errors.push({
+            msg: "Please enter a valid email"
+        })
+    }
+
+    if (password != confirmpassword) {
+        errors.push({
+            msg: "Both passwords are different. Kindly enter same password in the Confirm Password field."
+        })
+    }
+
+    if (errors.length > 0) {
+        req.flash("error", errors[0].msg);
+        res.redirect("/dashboard/" + req.params.id + "/edituser");
+    } else {
+        User.findByIdAndRemove(req.params.id, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Updating user...");
+            }
+        });
+        var newUser = new User({
+            username: req.body.username,
+            lastName: req.body.lastName,
+            firstName: req.body.firstName,
+            email: req.body.email,
+        });
+        if (req.body.role === "admin") {
+            newUser.isAdmin = "true"
+        }
+        User.register(newUser, req.body.password, function (err) {
+            if (err) {
+                console.log(err);
+                return res.redirect("/dashboard")
+            }
+            req.flash("success", "Successfully updated user: " + req.body.username);
+            res.redirect("/dashboard");
+        });
+    }
+});
+
+
+
+//delete USER
+app.delete("/dashboard/:id", isLoggedIn, function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            req.flash("error", "Could not remove user");
+            res.redirect("/dashboard");
+        } else {
+            req.flash("success", "Removed user");
+            res.redirect("/dashboard");
+        }
+    });
+});
+
+app.get("/profile/:id", function (req, res) {
+    res.render("profile", {
+        currentUser: req.user
+    });
+});
 
 //show login form
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
     res.render("login");
-  });
-  
-  //handling login logic
-  app.post("/login", passport.authenticate("local", {
+});
+
+//handling login logic
+app.post("/login", passport.authenticate("local", {
     successRedirect: "/home",
     failureRedirect: "/login",
     failureFlash: true
-  }), function() {});
-  
-  //logout ROUTE
-  app.get("/logout", function(req, res) {
+}), function () {});
+
+//logout ROUTE
+app.get("/logout", function (req, res) {
     req.logout();
     req.flash("success", "Successfully logged out");
     res.redirect("/login");
-  });
+});
 
-  //isloggedin
+
+
+//isloggedin
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
-      return next();
+        return next();
     }
     req.flash("error", "Please log in first")
     res.redirect("/login");
-  }
-  
+}
+
 
 app.listen(3000,function(){
-  console.log("sv is up");
+  console.log("server is up");
 });
